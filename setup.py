c85ab0df442cacf7b2f8c4ebcf3bae735ed5f146
Samuel Kolb <samuel.kolb@me.com>
2018-08-29 15:08:33 +0200

    Updated setup to find all packages

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Distribute: python setup.py sdist upload
 
 setup(
     name='pywmi',
-    version='0.2.8',
+    version='0.2.9',
     description='Essential tools and interfaces for WMI',
     url='http://github.com/samuelkolb/pywmi',
     author='Samuel Kolb',
     author_email='samuel.kolb@me.com',
     license='MIT',
-    packages=['pywmi'],
+    packages=find_packages(),
     zip_safe=False,
     install_requires=['pysmt', 'numpy', 'future'],
     setup_requires=['pytest-runner'],
