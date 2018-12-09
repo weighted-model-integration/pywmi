bef18ae1b31499c2f2660efc376a6eb9856f38b9
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2018-12-09 12:25:58 +0100

    up to semiring works

--- pywmi/engines/xsdd/xsdd.py
+++ pywmi/engines/xsdd/xsdd.py
@@ -11,21 +11,42 @@ from pysmt.shortcuts import Real, TRUE
 from pywmi.domain import TemporaryDensityFile
 from pywmi.engine import Engine
 
+from .smt2pl import SMT2PL
+
+
+from problog.cycles import break_cycles
+from problog.evaluator import Semiring
+
+import hal_problog.utils
+from hal_problog.solver import SumOperator, InferenceSolver
+from hal_problog.formula import LogicFormula
+
+
 logger = logging.getLogger(__name__)
 
 
-class XsddEngine(Engine):
+class XsddEngine(Engine, SMT2PL):
     def __init__(self, domain, support, weight, mode=None, timeout=None):
-        super().__init__(domain, support, weight)
+        Engine.__init__(self, domain, support, weight)
+        SMT2PL.__init__(self, domain, support, weight)
+        self.problog_program = hal_problog.utils.load_string(self.string_program)
+
         self.mode = mode
         self.timeout = timeout
 
 
-    def call_wmi(self, queries=None, timeout=None):
-        print(self.domain)
-        print(self.support)
-        print(self.weight)
-        print("")
+        self.solver = InferenceSolver(abe="psi")
+
+    def call_wmi(self, queries=None, timeout=None, **kwdargs):
+        lf_hal, _, _, _ = self.solver.ground(self.problog_program, queries=None, **kwdargs)
+        lf = break_cycles(lf_hal, LogicFormula(**kwdargs))
+        operator = SumOperator()
+        semiring = SemiringWMI(operator.get_neutral(), self.solver.abe)
+        diagram = self.solver.compile_formula(lf, **kwdargs)
+        dde = diagram.get_evaluator(semiring=semiring, **kwdargs)
+        # dde.formula.density_values = density_values
+        # sdds = dde.get_sdds()
+
 
         return [1]
 
@@ -39,6 +60,10 @@ class XsddEngine(Engine):
         else:
             return result[0]
 
+    def compute_probabilities(self, queries):
+        volume = self.compute_volume()
+        return [volume for q in queries]
+
 
     def copy(self, support, weight):
         return XsddEngine(self.domain, support, weight, self.mode, self.timeout)
@@ -49,3 +74,17 @@ class XsddEngine(Engine):
         if self.timeout is not None:
             result += ":t{}".format(self.timeout)
         return result
+
+
+
+class SemiringWMI(Semiring):
+    def __init__(self, neutral, abe):
+        self.neutral = neutral
+        self.abe = abe
+
+    def negate(self, a):
+        print(a)
+        return 0
+    def value(self,a):
+        # print(a)
+        return 1
