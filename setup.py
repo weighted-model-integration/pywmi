c7c4ce960d3e49c6cb475eae4a58183c90051939
Samuel Kolb <samuel.kolb@me.com>
2019-02-01 01:52:04 +0100

    - Upped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pywmi',
-    version='0.3.17',
+    version='0.3.18',
     description='Essential tools and interfaces for WMI',
     url='http://github.com/samuelkolb/pywmi',
     author='Samuel Kolb',
