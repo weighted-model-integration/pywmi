fb0e5042ad265b67b50cf9e4767216439062a748
Samuel Kolb <samuel.kolb@me.com>
2019-02-02 13:19:27 +0100

    Revamped setup.py file

--- setup.py
+++ setup.py
@@ -1,33 +1,94 @@
-from setuptools import setup, find_packages
+import os
+import shutil
+import sys
+
+from setuptools import setup, find_packages, Command
 from os import path
 
-# Distribute: python setup.py sdist upload
-# python setup.py sdist bdist_wheel
-# twine upload dist/*
-# rm -rf dist/
-# rm -rf build/
+NAME = 'pywmi'
+DESCRIPTION = 'Essential tools and interfaces for WMI solving.'
+URL = 'http://github.com/samuelkolb/pywmi'
+EMAIL = 'samuel.kolb@me.com'
+AUTHOR = 'Samuel Kolb'
+REQUIRES_PYTHON = '>=3.5.0'
+VERSION = "0.4.5"
 
-setup_dir = path.abspath(path.dirname(__file__))
-with open(path.join(setup_dir, "README.md")) as ref:
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'pysmt<0.8', 'numpy', 'future', 'typing', 'matplotlib', 'pillow', 'polytope', 'tabulate', 'graphviz', 'sympy',
+    'scipy', 'autodora>=0.3.3', 'deprecated', 'networkx'
+]
+
+# What packages are optional?
+EXTRAS = {
+        'sdd': ["pysdd"]
+}
+
+# Distribute: python setup.py sdist upload
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(path.join(here, "README.md")) as ref:
     long_description = ref.read()
 
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            shutil.rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        # self.status('Pushing git tags…')
+        # os.system('git tag v{0}'.format(about['__version__']))
+        # os.system('git push --tags')
+
+        sys.exit()
+
+
 setup(
-    name='pywmi',
-    version='0.4.4',
-    description='Essential tools and interfaces for WMI',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/samuelkolb/pywmi',
-    author='Samuel Kolb',
-    author_email='samuel.kolb@me.com',
+    url=URL,
+    author=AUTHOR,
+    author_email=EMAIL,
     license='MIT',
-    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(exclude=('tests',)),
     zip_safe=False,
-    install_requires=['pysmt<0.8', 'numpy', 'future', 'typing', 'matplotlib', 'pillow', 'polytope', 'tabulate',
-                      'graphviz', 'sympy', 'scipy', 'autodora>=0.3.3', 'deprecated', 'networkx'],
-    extras_require={
-        'sdd': ["pysdd"]
-    },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     setup_requires=['pytest-runner'],
     tests_require=["pytest"],
     entry_points={
@@ -35,5 +96,8 @@ setup(
             "pywmi-install = pywmi.install:main",
             "pywmi-cli = pywmi.__main__:parse"
         ]
-    }
+    },
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
