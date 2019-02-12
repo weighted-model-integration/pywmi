b6559cc7e112bc1d8241fcf1744f6c0625c3020d
Samuel Kolb <samuel.kolb@me.com>
2019-02-12 15:35:59 +0100

    Improved pretty printing

--- pywmi/smt_print.py
+++ pywmi/smt_print.py
@@ -1,3 +1,4 @@
+from .smt_math import LinearInequality
 from .smt_walk import SmtWalker
 import pysmt.shortcuts as smt
 
@@ -10,13 +11,23 @@ class PrintWalker(SmtWalker):
         return "(" + " | ".join(self.walk_smt(p) for p in args) + ")"
 
     def walk_plus(self, args):
-        return " + ".join(self.walk_smt(p) for p in args)
+        args = [self.walk_smt(p) for p in args]
+        if all(arg == "0" for arg in args):
+            return "0"
+        args = [arg for arg in args if arg != "0"]
+        return " + ".join(args)
 
     def walk_minus(self, left, right):
         return "{} - {}".format(*[self.walk_smt(p) for p in [left, right]])
 
     def walk_times(self, args):
-        return "*".join(self.walk_smt(p) for p in args)
+        args = [self.walk_smt(p) for p in args]
+        if any(arg == "0" for arg in args):
+            return "0"
+        if all(arg == "1" for arg in args):
+            return "1"
+        args = [arg for arg in args if arg != "1"]
+        return "*".join(args)
 
     def walk_not(self, argument):
         return "~{}".format(self.walk_smt(argument))
@@ -28,10 +39,14 @@ class PrintWalker(SmtWalker):
         return "{}^{}".format(*[self.walk_smt(p) for p in [base, exponent]])
 
     def walk_lte(self, left, right):
-        return "({} <= {})".format(*[self.walk_smt(p) for p in [left, right]])
+        inequality = LinearInequality.from_smt(left <= right).normalize().to_smt()
+        left, right = [self.walk_smt(p) for p in inequality.args()]
+        return "({} <= {})".format(left, right)
 
     def walk_lt(self, left, right):
-        return "({} < {})".format(*[self.walk_smt(p) for p in [left, right]])
+        inequality = LinearInequality.from_smt(left <= right).normalize().to_smt()
+        left, right = [self.walk_smt(p) for p in inequality.args()]
+        return "({} < {})".format(left, right)
 
     def walk_equals(self, left, right):
         return "({} = {})".format(*[self.walk_smt(p) for p in [left, right]])
@@ -43,7 +58,11 @@ class PrintWalker(SmtWalker):
         if v_type == smt.BOOL:
             return str(value)
         elif v_type == smt.REAL:
-            return "{}".format(float(value))
+            if int(value) == float(value):
+                value = int(value)
+            else:
+                value = float(value)
+            return "{}".format(value)
         else:
             raise RuntimeError("Unknown type {}".format(v_type))
 
