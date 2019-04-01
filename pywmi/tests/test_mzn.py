ae8b26b04bec52671681605616f2ea11c97e6e5a
paolomorettin <paolo.morettin@unitn.it>
2019-04-01 17:01:05 +0200

    new mzn test

--- pywmi/tests/test_mzn.py
+++ pywmi/tests/test_mzn.py
@@ -1,4 +1,5 @@
 
+from pysmt.shortcuts import *
 from pywmi import Domain
 from pywmi.errors import ParsingFileError
 from pywmi.parser import MinizincParser#, SmtlibParser
@@ -16,13 +17,43 @@ def test_parse_error():
     content = "this should raise a ParsingFileError"
     tmp = temporary_file(content)
     try:
-        # support, weights, domA, domX, queries = MinizincParser.parseAll(tmp.name)
         _ = MinizincParser.parseAll(tmp.name)
         assert False
     except ParsingFileError:
         assert True
-    finally:
-        #tmp.close()
-        pass
 
+def test_mzn1():
+    content = """
+var float:x;
+var 0.0..2.0:y;
 
+weight : if y < 1 then x+y else 2*y endif;
+
+constraint (  y<1       -> (0<x /\ x<=2)  )
+        /\ (  not (y<1) -> (1<x /\ x<3)  );
+        
+query (x>1.5);
+query (x<=1.5);
+query (true);
+"""
+    tmp = temporary_file(content)
+
+    support, weights, domA, domX, queries = MinizincParser.parseAll(tmp.name)
+
+    x, y = sorted(domX.keys(), key=lambda x : x.symbol_name())
+    chi = And(Implies(LT(y, Real(1)), And(LT(Real(0), x), LE(x, Real(2)))),
+              Implies(Not(LT(y, Real(1))), And(LT(Real(1), x), LT(x, Real(3)))))
+
+    assert chi == support
+
+    w = Ite(LT(y, Real(1)),
+            Plus(x, y),
+            Times(Real(2),y))
+
+    assert w == weights
+        
+    phi1 = GE(x, Real(1.5))
+    phi2 = LE(x, Real(1.5))
+    phi3 = Bool(True)
+
+    assert queries == [phi1, phi2, phi3]
